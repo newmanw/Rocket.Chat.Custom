4a78422ad5c08d8167b8d20336b7814130aad7a3
William Newman <newmanw10@gmail.com>
2021-03-01 14:48:09 -0700

    ios

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Rocket.Chat.Custom
+
+mil.dod.nga.rocketchat
+xcodebuild -workspace mapcache-ios.xcworkspace -scheme mapcache-ios -allowProvisioningUpdates -configuration AppStoreDistribution archive -archivePath buildArchive/MapCache.xcarchive CODE_SIGNING_ALLOWED=NO
+xcodebuild -workspace mapcache-ios.xcworkspace -scheme mapcache-ios -allowProvisioningUpdates -configuration AppStoreDistribution archive -archivePath buildArchive/MapCache.xcarchive CODE_SIGNING_ALLOWED=NO
--- scripts/ios/plist.sh
+++ scripts/ios/plist.sh
@@ -2,10 +2,16 @@
 cd $1
 
 echo "Update RocketChatRN/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :AppGroup mil.dod.nga.rocketchat" ios/RocketChatRN/Info.plist
 /usr/libexec/PlistBuddy -c "Set :CFBundleDisplayName Comms" ios/RocketChatRN/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier mil.dod.nga.rocketchat" ios/RocketChatRN/Info.plist
 
 echo "Update ShareRocketChatRN/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :AppGroup mil.dod.nga.rocketchat" ios/ShareRocketChatRN/Info.plist
 /usr/libexec/PlistBuddy -c "Set :CFBundleDisplayName Comms Extension" ios/ShareRocketChatRN/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier mil.dod.nga.rocketchat.ShareExtension" ios/ShareRocketChatRN/Info.plist
 
 echo "Update NotificationService/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :AppGroup mil.dod.nga.rocketchat" ios/NotificationService/Info.plist
 /usr/libexec/PlistBuddy -c "Set :CFBundleDisplayName Comms NotificationService" ios/NotificationService/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier mil.dod.nga.rocketchat.NotificationService" ios/NotificationService/Info.plist
