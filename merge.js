43bf335cca8261ef6cae766d0a8d4811e86b3b36
William Newman <newmanw10@gmail.com>
2021-04-07 12:30:02 -0600

    More

--- merge.js
+++ merge.js
@@ -4,27 +4,32 @@ const path = require('path');
 appPath = process.argv.slice(2)[0];
 
 // app.json
-const appJsonPath = `${appPath}${path.sep}app.json`
+// const appJsonPath = `${appPath}${path.sep}app.json`
 
 // TODO validate against gold copy
-const appJson = require(appJsonPath);
-appJson.server = 'https://rocketchat.stage.geointservices.io';
-appJson.appGroup = 'group.mil.dod.nga.rocketchat';
-appJson.appStoreID = '9999';
-fs.writeFileSync(appJsonPath, JSON.stringify(appJson));
+// const appJson = require(appJsonPath);
+// appJson.server = 'https://rocketchat.stage.geointservices.io/';
+// appJson.appGroup = 'group.mil.dod.nga.rocketchat';
+// appJson.appStoreID = '9999';
+// fs.writeFileSync(appJsonPath, JSON.stringify(appJson));
 
 // colors.js
 // TODO validate against gold copy
-const colorsSrc = path.join('Rocket.Chat.ReactNative', 'app', 'constants', 'colors.js');
-const colorsDest = path.join(appPath, 'app', 'constants', 'colors.js');
-fs.copyFileSync(colorsSrc, colorsDest);
+// const colorsSrc = path.join('Rocket.Chat.ReactNative', 'app', 'constants', 'colors.js');
+// const colorsDest = path.join(appPath, 'app', 'constants', 'colors.js');
+// fs.copyFileSync(colorsSrc, colorsDest);
+
+// App Resources
+const appSrc = path.join('Rocket.Chat.ReactNative');
+const appDest = path.join(appPath);
+fs.copySync(appSrc, appDest);
 
 // Android resources
-const androidSrc = path.join('Rocket.Chat.ReactNative', 'android');
-const androidDest = path.join(appPath, 'android');
-fs.copySync(androidSrc, androidDest);
+// const androidSrc = path.join('Rocket.Chat.ReactNative', 'android');
+// const androidDest = path.join(appPath, 'android');
+// fs.copySync(androidSrc, androidDest);
 
 // iOS resources
-const iosSrc = path.join('Rocket.Chat.ReactNative', 'ios',);
-const iosDest = path.join(appPath, 'ios');
-fs.copySync(iosSrc, iosDest);
+// const iosSrc = path.join('Rocket.Chat.ReactNative', 'ios',);
+// const iosDest = path.join(appPath, 'ios');
+// fs.copySync(iosSrc, iosDest);
